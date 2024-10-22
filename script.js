a6d77945ccceeaac13cfd20b81985e7ee9f96813
Kraken57 <kasaad1609@gmail.com>
2024-10-23 02:14:10 +0530

    stagger, repeat and yoyo in gsap

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 gsap.to("#box1", {
-  x: 1000,
+  x: 1200,
   duration: 2,
   delay: 1,
   rotate: 360,
   backgroundColor: "pink",
   borderRadius: "50%",
+  repeat: -1,
+  yoyo: true,
 });
 
 gsap.from("#box2", {
-  x: 1000,
+  x: 1200,
   duration: 2,
   delay: 1,
   rotate: 360,
   borderRadius: "50%",
   scale: 0.5,
+  repeat: -1, //for infinite repeat
+  yoyo: true, //for reverse animation
 });
 
 gsap.from("h1", {
@@ -22,4 +26,5 @@ gsap.from("h1", {
   y: 30,
   duration: 2,
   delay: 1,
+  stagger: 0.3,  // negative value will make the animation start opposite direction
 });
