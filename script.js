2749b9ffd4838ab5027a684fd501c66bf333036f
Kraken57 <kasaad1609@gmail.com>
2024-10-23 02:05:44 +0530

    text-animation

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ gsap.to("#box1", {
   rotate: 360,
   backgroundColor: "pink",
   borderRadius: "50%",
-
 });
 
 gsap.from("#box2", {
@@ -15,4 +14,12 @@ gsap.from("#box2", {
   rotate: 360,
   borderRadius: "50%",
   scale: 0.5,
+});
+
+gsap.from("h1", {
+  opacity: 0,
+  color: "yellow",
+  y: 30,
+  duration: 2,
+  delay: 1,
 });
